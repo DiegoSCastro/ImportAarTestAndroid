aa7373f94fcc9f4e89c4b6df86884a5a36059f7c
Diego Castro <omegadiegocastro@gmail.com>
2024-07-21 16:50:49 -0300

    feat(main activity): add print button

--- app/src/main/java/com/diegocastro/importaartestandroid/MainActivity.kt
+++ app/src/main/java/com/diegocastro/importaartestandroid/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.diegocastro.importaartestandroid
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,6 +19,8 @@ import androidx.compose.ui.unit.dp
 import com.diegocastro.importaartestandroid.ui.theme.ImportAarTestAndroidTheme
 import com.mercadolibre.android.point_integration_sdk.nativesdk.MPManager
 import com.mercadolibre.android.point_integration_sdk.nativesdk.configurable.MPConfigBuilder
+import com.mercadolibre.android.point_integration_sdk.nativesdk.message.utils.doIfError
+import com.mercadolibre.android.point_integration_sdk.nativesdk.message.utils.doIfSuccess
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +32,8 @@ class MainActivity : ComponentActivity() {
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding),
-                        onInitializeMPManager = { initializeMPManager() }
+                        onInitializeMPManager = { initializeMPManager() },
+                        onPrint = { printImageBitmap() }
                     )
                 }
             }
@@ -42,17 +47,37 @@ class MainActivity : ComponentActivity() {
             .build()
         MPManager.initialize(application, config)
     }
+    private fun printImageBitmap() {
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable
+            .point_mainapp_demo_app_ic_datafono)
+
+        MPManager.bitmapPrinter.print(bitmap) { response ->
+            response
+                .doIfSuccess { printResult ->
+                    Log.d("MiniAppPlugin", "Bitmap printed successfully: $printResult")
+//                    result.success("Bitmap printed successfully: $printResult")
+                }
+                .doIfError { error ->
+                    Log.e("MiniAppPlugin", "Failed to print bitmap: ${error.message}")
+//                    result.error("PRINT_ERROR", "Failed to print bitmap: ${error.message}", null)
+                }
+        }
+    }
 }
 
 @Composable
 fun Greeting(
     name: String,
     modifier: Modifier = Modifier,
-    onInitializeMPManager: () -> Unit
+    onInitializeMPManager: () -> Unit,
+    onPrint: () -> Unit
 ) {
     Column(modifier = modifier.fillMaxSize()) {
         Button(onClick = onInitializeMPManager, modifier = Modifier.padding(16.dp)) {
             Text(text = "Initialize MPManager")
+        }
+        Button(onClick = onPrint, modifier = Modifier.padding(16.dp)) {
+            Text(text = "Print Image Bitmap")
         }
         Text(
             text = "Hello $name!",
@@ -65,6 +90,6 @@ fun Greeting(
 @Composable
 fun GreetingPreview() {
     ImportAarTestAndroidTheme {
-        Greeting("Android", onInitializeMPManager = {} )
+        Greeting("Android", onInitializeMPManager = {}, onPrint = {})
     }
 }
